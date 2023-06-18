b682bee87081f5f891efabbd4762cb60f2ca48a2
Saniul Islam Sani <saniulislam58@gmail.com>
2023-06-19 03:14:31 +0600

    primary

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="members">
+      <h1 style={{textAlign:"center", color:"white", fontWeight:"700"}}>
+        Welcome to NovaScriptors 
+        <br/><br/>
+        Explore the world of Robotics with us!
+        </h1>
+        <br/><br/>
+        <p className='description'>
+        NovaScriptors is a Robotics Club of Independent University, Bangladesh.
+        <br/><br/>
+        We are a group of passionate students who are working on different projects related to Robotics.
+        <br/><br/>
+        We are also working on different research papers and publications.
+        <br/><br/>
+        This website is a part of our project of <span style={{fontSize:"20px", fontWeight:"bold"}}>IoT based Hybrid Renewable Energy System</span>
+        </p>
+
+    </div>
+  )
+}
+
+export default Home
